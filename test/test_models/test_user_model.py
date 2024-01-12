790fb713fd2fc50125559dca805ca3d303cd775c
Librazionpc <nwajeizion393@gmail.com>
2024-01-12 20:51:30 +0100

    Modified the other models and added the test file

--- test/test_models/test_user_model.py
+++ test/test_models/test_user_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Scripts that tests for User 
+# Scripts that tests for User
+
 import sys
 import unittest
 sys.path.append("../../")
@@ -13,7 +14,7 @@ Review = review.Review
 
 class TestModels(unittest.TestCase):
     def setUp(self):
-        # Create instances for testing
+        # Create instances for testing ifr all the classes
         self.user = User(
             email="test@example.com",
             password="password123",
@@ -27,27 +28,42 @@ class TestModels(unittest.TestCase):
         self.review = Review(place_id="1", user_id="1", text="Great stay!")
     
     def test_user_attributes(self):
+        """Test for the user entries
+        """
         self.assertEqual(self.user.email, "test@example.com")
         self.assertEqual(self.user.password, "password123")
         self.assertEqual(self.user.first_name, "John")
         self.assertEqual(self.user.last_name, "Doe")
     
     def test_state_attributes(self):
+        """ Test for the state user entris
+        """
         self.assertEqual(self.state.name, "California")
 
     def test_city_attributes(self):
+        """Test for all the City entries
+        """
         self.assertEqual(self.city.state_id, "1")
         self.assertEqual(self.city.name, "San Francisco")
 
     def test_amenity_attributes(self):
+        """Test for all amenity entries
+        """
         self.assertEqual(self.amenity.name, "WiFi")
 
     def test_place_attributes(self):
+        """Test for all place entries
+        """
         self.assertEqual(self.place.city_id, "1")
         self.assertEqual(self.place.user_id, "1")
         self.assertEqual(self.place.name, "Cozy Apartment")
 
     def test_review_attributes(self):
+        """Tests for all review entries
+        """
         self.assertEqual(self.review.place_id, "1")
         self.assertEqual(self.review.user_id, "1")
         self.assertEqual(self.review.text, "Great stay!")
+        
+if __name__ == "__main__":
+    unittest.main()

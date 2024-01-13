b756bd4f8d3ab0d2528a14a375dd55481cc31e50
Librazionpc <nwajeizion393@gmail.com>
2024-01-13 20:07:36 +0100

    Checker BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Contains a single Class "BaseModel" which serves has the base model for
-    all the model that will be created and it also contain methods serialize and
-    deserialize the object 
+"""Scripts that deals with the Class [BaseModel]
+   That also create the dict for all object for
+   serialization and deserialization
 """
 from datetime import datetime
 import json
 import models
 from uuid import uuid4
 
+
 class BaseModel():
-    """Handles the serialization and deserialization of the object 
+    """Handles the serialization and deserialization of the object
     """
+
     def __init__(self, *arg, **kwargs):
         """initialize the BaseModel
         """
@@ -27,26 +29,25 @@ class BaseModel():
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-            
+
     def save(self):
-        """_summary_
-        saves the object to a file in form of json 
+        """_summary_: saves the object to a file in form of json
         """
         self.updated_at = datetime.now()
         models.storage.save()
-        
-        
+
     def to_dict(self):
-        """_summary_: returns the dictionary representation of an instance       
+        """_summary_: returns the dictionary representation of an instance
         """
         new_dict = {key: value for key, value in self.__dict__.items()}
         new_dict["created_at"] = new_dict["created_at"].isoformat()
         new_dict["updated_at"] = new_dict["updated_at"].isoformat()
         new_dict["__class__"] = self.__class__.__name__
         return new_dict
+
     def __str__(self):
         """ Retuns a string representation of an instance
         Returns:
-            _type_:_str
+            _type_:__str__
         """
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}], ({self.id}), {self.__dict__}"
